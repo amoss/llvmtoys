a3725539af30ceea2604ae6f863363c6e07bfb2e
Andrew Moss <1043609+amoss@users.noreply.github.com>
2021-01-07 11:43:50 +0100

    Fix linux config so that it compiles automatically.
    Switch off the key signing for modules.

--- tests/linux/prepare.sh
+++ tests/linux/prepare.sh
@@ -6,8 +6,22 @@ pushd $BASE
 [[ -d linux.git ]] || git clone ssh://git@github.com/torvalds/linux linux.git
 cd linux.git
 
+git reset
+git clean -dxf
+make defconfig
 make clean
-make --trace >>../make.trace
+
+for x in "CONFIG_SYSTEM_TRUSTED_KEYRING" ; do
+  echo $x
+  sed -i -e 's/^'$x'/#'$x'/' .config
+  echo "$x=n" >>.config
+done
+for x in "CONFIG_SYSTEM_TRUSTED_KEYS" ; do
+  echo $x
+  sed -i -e 's/^'$x'/#'$x'/' .config
+  echo $x'=""' >>.config
+done
+make --trace | tee ../make.trace
 
 popd
 
